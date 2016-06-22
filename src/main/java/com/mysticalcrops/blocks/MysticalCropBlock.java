d8db014b81c935882600048fc8f7eb9888df0541
SwordKorn20 <swordkorn@live.com>
2016-06-22 17:05:19 +0100

    Added jsons for the inventory variant to shut the damn log up...

--- src/main/java/com/mysticalcrops/blocks/MysticalCropBlock.java
+++ src/main/java/com/mysticalcrops/blocks/MysticalCropBlock.java
@@ -40,6 +40,7 @@ public class MysticalCropBlock extends BlockCrops implements IGrowable, IPlantab
     	super();
         this.regName = regName;
         this.setDefaultState(blockState.getBaseState().withProperty(AGE, 0));
+        this.setCreativeTab(MysticalCrops.cropsTab);
     }
 
     public boolean isSuitableForPlant(Block soil) {
